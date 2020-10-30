7a96acd500787aed0327645b14d21f58e6943cea
wafi <mameri.wafi@gmail.com>
2020-10-30 20:13:56 +0100

    add staticfiles

--- medical/settings.py
+++ medical/settings.py
@@ -148,12 +148,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login_view'

920f45ed5419502cc74c525458568e8b6156a3dd
wafi <mameri.wafi@gmail.com>
2020-10-30 20:36:24 +0100

    add staticfiles

--- medical/settings.py
+++ medical/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +164,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login_view'
 # AUTH_USER_MODEL = 'accounts.CostumUser' 
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
